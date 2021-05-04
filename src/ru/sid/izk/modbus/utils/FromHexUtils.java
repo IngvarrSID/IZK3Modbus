b8023d3d25e66fce877c83e318bd48ecb46be5a7
aprakhov <7aprakhov@gmail.com>
2021-05-05 00:34:16 +0300

    removed useless method in FromHexUtils.java

--- src/ru/sid/izk/modbus/utils/FromHexUtils.java
+++ src/ru/sid/izk/modbus/utils/FromHexUtils.java
@@ -2,12 +2,7 @@ package ru.sid.izk.modbus.utils;
 
 public final class FromHexUtils {
 
-    //TODO method with int argument used only here, remove it.
-    public static String stringFromHex(int n) {
-        return String.format("0x%8s", Integer.toHexString(n)).replace(' ', '0');
-    }
-
     public static String stringFromHex(float f) {
-        return stringFromHex(Float.floatToIntBits(f));
+        return String.format("0x%8s", Integer.toHexString(Float.floatToIntBits(f))).replace(' ', '0');
     }
 }
